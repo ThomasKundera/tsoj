6d8895958eb1bdfd569084bcacf85ae4197c7001
TK <thomas.kundera@yahoo.fr>
2026-04-14 12:52:26 +0200

    Works

--- code/bash/run_render.sh
+++ code/bash/run_render.sh
@@ -5,6 +5,11 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Projectdir is two up
 export PROJECTDIR="$( cd $SCRIPTDIR/../.. >/dev/null 2>&1 && pwd )"
 
+export PYTHONDIR=$PROJECTDIR/code/blender
+
+export PYTHONPATH=$PYTHONPATH:$PYTHONDIR
+export BLENDER_USER_SCRIPTS=$PYTHONPATH:$PYTHONDIR
+
 export WORKDIR=$PROJECTDIR/output
 mkdir -p $WORKDIR
 
@@ -12,5 +17,6 @@ echo "Working in $WORKDIR"
 
 cd $WORKDIR
 
-blender -b -P $PROJECTDIR/code/blender/myscene.py
+blender --python-use-system-env -b -P $PROJECTDIR/code/blender/myscene.py
 
+echo "Done"
